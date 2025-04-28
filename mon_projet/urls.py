b5cb642411c14714cd016dd2573d9b28217f7e7c
Libasse SEYE <mamelibasselayeseye@esp.sn>
2025-04-28 16:20:13 +0200

    Initialisation du projet Django

--- mon_projet/urls.py
+++ mon_projet/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for AgriSEN project.
+URL configuration for mon_projet project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- mon_projet/wsgi.py
+++ mon_projet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AgriSEN project.
+WSGI config for mon_projet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgriSEN.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mon_projet.settings')
 
 application = get_wsgi_application()
