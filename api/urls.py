32804d086b1f42bb564da1eb6aa21cc873b61d77
n0reyni <ousmanesow7@esp.sn>
2025-04-29 19:27:15 +0000

    added new endpoint /api/sujet-forum/

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('sujet_forum/', SujetForumList.as_view(), name='sujet-list'),
-    path('sujet_forum/<uuid:pk>/', SujetForumDetails.as_view(), name='sujet-forum-details'),
+    path('sujet-forum/', SujetForumList.as_view(), name='sujet-list'),
+    path('sujet-forum/<uuid:pk>/', SujetForumDetails.as_view(), name='sujet-forum-details'),
 ]
