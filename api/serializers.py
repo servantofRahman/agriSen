7719f74c2936a56ba171cb876a365f02c591aee2
Libasse SEYE <mamelibasselayeseye@esp.sn>
2025-05-06 00:40:12 +0200

    Ajout de trois endpoints pour la messagerie

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from .views import *
+# from .views import *
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth.models import User
@@ -12,4 +12,26 @@ class PublicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = publications
         fields = ['publication_id', 'user_id', 'contenu', 'image', 'date_publication', 'region', 'culture_associee']
+
+class CommentairesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = commentaires
+        fields = ['commentaire_id', 'publication_id', 'user_id', 'contenu', 'date_commentaire']
+
+class Message_priveesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = messages_privees
+        fields ='__all__' 
+
+class Messages_forumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = messages_forum
+        fields = '__all__'
+
+
+
+
+
+
+
         
--- api/views.py
+++ api/views.py
@@ -23,4 +23,35 @@ class PublicationDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = publications.objects.all()
     serializer_class = PublicationSerializer
     lookup_field = 'pk'
-    
+
+class CommentairesListCreateView(generics.ListCreateAPIView):
+    queryset = commentaires.objects.all()
+    serializer_class = CommentairesSerializer
+
+class CommentairesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = commentaires.objects.all()
+    serializer_class = CommentairesSerializer
+    lookup_field = 'pk'
+
+class Messages_priveesListCreateView(generics.ListCreateAPIView):
+    queryset = messages_privees.objects.all()
+    serializer_class = Messages_priveesSerializer
+
+class Messages_priveesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = messages_privees.objects.all()
+    serializer_class = Messages_priveesSerializer
+    lookup_field = 'pk'
+
+class Messages_forumListCreateView(generics.ListCreateAPIView):
+    queryset = messages_forum.objects.all()
+    serializer_class = Messages_forumSerializer
+
+class Messages_forumRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = messages_forum.objects.all()
+    serializer_class = Messages_forumSerializer
+    lookup_field = 'pk'
+
+
+
+
+
